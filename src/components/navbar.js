dc58e1a3ce899acf480b19497b17e00532d7a1c5
Abdelhak-Achour <achourabdelhak2001@gmail.com>
2023-12-08 05:26:05 +0100

    made navbar skeleton and added pages starters

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../images/logo.svg";
 
 export function Navbar()
 {
     return(
         <>
-            
+            <nav>
+                <Link to = "/"><img src = {logo} alt = "logo" /></Link>
+                <ul>
+                    <li>
+                        <NavLink to = "/">Acceuil</NavLink>
+                        <NavLink to = "/solutions">Solutions?</NavLink>
+                        <NavLink to = "/myths">Mythes</NavLink>
+                        <NavLink to = "/chat">Chatter avec nous</NavLink>
+                        <NavLink to = "/login">Se connecter</NavLink>
+                        <NavLink to = "/signup">S'inscrire</NavLink>
+                    </li>
+                </ul>
+            </nav>
         </>
     )
 }
---
+++ src/pages/myths.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function Myths()
+{
+    return(
+        <>
+            <h1>Myths page</h1>
+        </>
+    )
+}
